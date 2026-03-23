2afea1028f6fb048a902ac9c57d089cdc3adb4d7
Тимур <timurmakssudov@gmail.com>
2026-03-23 20:13:12 +0300

    конец

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ from reviews.reviews import leave_review
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     persistence = PicklePersistence(filepath="vpn_bot")
